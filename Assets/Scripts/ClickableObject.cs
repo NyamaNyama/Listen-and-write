b09d1ddd814db4339b997acf2143b24702a61a0f
Nyama N <tihon31.01.04@mail.ru>
2024-12-20 21:41:48 +1000

    Add basic logic for interacting with objects

---
+++ Assets/Scripts/ClickableObject.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ClickableObject: MonoBehaviour
+{
+
+    public void OnHoverEnter()
+    {
+        Debug.Log("навелся");
+    }
+    public void OnHoverExit()
+    {
+
+    }
+
+    public abstract void OnClick();
+}
---
+++ Assets/Scripts/InteractionManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionManager : MonoBehaviour
+{
+    private ClickableObject _currentHovered;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray, out RaycastHit hit))
+        {
+            if(hit.collider.TryGetComponent(out ClickableObject obj))
+            {
+                if(_currentHovered != obj)
+                {
+                    _currentHovered?.OnHoverExit();
+                    _currentHovered = obj;
+                    _currentHovered.OnHoverEnter();
+                }
+                if(Input.GetMouseButtonDown(0))
+                {
+                    _currentHovered.OnClick();
+                }
+            }
+
+            else
+            {
+                _currentHovered?.OnHoverExit();
+                _currentHovered = null;
+            }
+        }
+    }
+}
